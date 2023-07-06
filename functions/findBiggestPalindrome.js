e458ddaba4b9b34ff7a1a5265321d867f76fa5e1
maviYoki <mariavitoriapossi@hotmail.com>
2023-07-05 22:38:00 -0300

    Added biggest palindrome function

---
+++ functions/findBiggestPalindrome.js
@@ -0,0 +1,32 @@
+export default function findBiggestPalindrome(string) {
+   let biggestPalindrome = '';
+   let lastBiggestPalindromeSize = 0;
+
+   for (let i = 0; i < string.length; i++) {
+      for (let j = string.length; j > i; j--) {
+         let word = '';
+
+         // Palavra ao contrario
+         for (let k = j - 1; k >= i; k--) {
+            word += string[k];
+         }
+
+         let isPalindrome = true;
+
+         for (let m = 0; m < word.length; m++) {
+            if (word[m] !== string[i + m]) {
+               isPalindrome = false;
+               break;
+            }
+         }
+
+         if (isPalindrome && word.length > lastBiggestPalindromeSize) {
+            biggestPalindrome = word;
+            lastBiggestPalindromeSize = word.length;
+            break;
+         }
+      }
+   }
+
+   return biggestPalindrome;
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import findBiggestPalindrome from './functions/findBiggestPalindrome.js';
 import removeDuplicate from './functions/removeDuplicate.js';
 import reversePhrase from './functions/reversePhrase.js';
 
 console.log(reversePhrase('Hello, World! OpenAI is amazing.'));
 console.log(removeDuplicate('Hello, World!'));
+console.log(findBiggestPalindrome('babad'));
